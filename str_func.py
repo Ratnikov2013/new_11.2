39303b8da1eb1184a8846e1ff884a3f9c8d696c6
Ivan Ratnikov <ratnikov_iv@mail.ru>
2024-06-13 15:39:49 +0200

    add str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def func_upper(s):
+    '''Создаем docstring'''
     new_string = s.upper()
     return new_string
+
+def func_capital_letter(word):
+    """Ф-ция, которая превращает первые буквы слов в заглавные"""
+    return word.title()
